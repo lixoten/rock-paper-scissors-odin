078f4196bd2b81363a0393df63870528948f2ff5
lixoten <lixoten@gmail.com>
2024-04-10 14:31:33 -0700

    Revisited code, added helpers, loop, input validation, update the README file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rock-paper-scissors-odin
-Rock Paper Scissors Ptoject at The Odin Ptoject
+# Rock Paper Scissors Project at The Odin Project
+
+## Rock Paper Scissors - Part 1
+
+### Things I did
+* Used Constants . ROCK PAPER SCISSORS, prevent typos
+* Use Loop that runs 5 times
+* Validate user input. uses Array
+* Track number of Tie Rounds
+* Lazy me Helper. Takes "r", "p", "s"
+* Explain Win Helper Function
+
+This is still the First Pass, as in Not Revisited yet per Odin
--- main.js
+++ main.js
@@ -1,83 +1,109 @@
 const ROCK = "rock";
 const PAPER = "paper";
-const SCISSOR = "scissor";
+const SCISSORS = "scissors";
+
+const possibleMoveArray = [ROCK, PAPER, SCISSORS];
 
 function getComputerChoice() {
+
     let randNumber = Math.random() * 3;
     randNumber = Math.ceil(randNumber);
 
-    if (randNumber === 1) {
-        return "rock";
-    } else if (randNumber === 2) {
-        return "paper";
-    } else if (randNumber === 3) {
-        return "scissor";
-    } else  {
-        console.log("OOOPPSS11111")
-        return `Opps ${randNumber}`
-    }
+    return possibleMoveArray[randNumber - 1]
 }
 
-function playRound(playerSelection , computerSelection) {
+function playRound(playerSelection , computerSelection, currentRound) {
 
-    console.log(`player ${playerSelection} - computer ${computerSelection}`)
-    if (playerSelection === ROCK && computerSelection === SCISSOR) {
-        playerWins++;
-        return "playerWins"; //rock breaks scissors
-    }
-    if (playerSelection === ROCK && computerSelection === PAPER) {
-        computerWins++;
-        return "computerWins"; //paper covers rock.
+    console.log(`**** Round ${currentRound} ****`)
+    console.log(`--- Move by the player:  ${playerSelection}`)
+    console.log(`--- Move by the computer:  ${computerSelection}`)
+
+    if (!possibleMoveArray.includes(playerSelection)) {
+        return `Invalid Move : ${playerSelection}`;
     }
 
-    if (playerSelection === PAPER && computerSelection === ROCK) {
-        playerWins++;
-        return "playerWins"; //paper covers rock.
-    }
-    if (playerSelection === PAPER && computerSelection === SCISSOR) {
-        computerWins++;
-        return "computerWins"; //scissors cuts paper
-    }
-    if (playerSelection === SCISSOR && computerSelection === PAPER) {
-        playerWins++;
-        return "playerWins"; //scissors cuts paper
-    }
-    if (playerSelection === SCISSOR && computerSelection === ROCK) {
-        computerWins++;
-        return "computerWins"; //rock breaks scissors
-    }
 
     if (playerSelection === computerSelection) {
+        tieCount++;
         return "Tie try again";
     }
+
+    const explanation = explainWin(playerSelection, computerSelection);
+
+    if ((playerSelection === ROCK && computerSelection === SCISSORS) ||
+        (playerSelection === PAPER && computerSelection === ROCK) ||
+        (playerSelection === SCISSORS && computerSelection === PAPER)) {
+        playerWins++;
+        return `Player Wins: ${explanation}\n`;
+    } else {
+        computerWins++;
+        return `Computer Wins: ${explanation}\n`;
+    }
 }
+
+function explainWin(playerSelection, computerSelection) {
+    if ((playerSelection === ROCK && computerSelection === SCISSORS) ||
+        (playerSelection === SCISSORS && computerSelection === ROCK)) {
+        return "Rock breaks Scissors";
+    }
+    if ((playerSelection === ROCK && computerSelection === PAPER) ||
+        (playerSelection === PAPER && computerSelection === ROCK)) {
+        return "Paper covers Rock.";
+    }
+    if ((playerSelection === PAPER && computerSelection === SCISSORS) ||
+        (playerSelection === SCISSORS && computerSelection === PAPER)) {
+        return "Scissors cuts Paper";
+    }
+}
+
+function getUserPrompt() {
+    let userPrompt = prompt("Enter your move: ").toLowerCase();
+
+    if ((userPrompt === ROCK) ||
+        (userPrompt === PAPER) ||
+        (userPrompt === SCISSORS)){
+        return userPrompt;
+    }
+
+    // Since I am lazy
+    if (userPrompt === 'r') {
+        return userPrompt = ROCK;
+    } else if (userPrompt === 'p') {
+        return userPrompt = PAPER;
+    } else if (userPrompt === 's') {
+        return userPrompt = SCISSORS;
+    } else {
+        return userPrompt;
+    }
+}
+
+const roundsToPlay = 5;
+
 let playerWins = 0;
 let computerWins = 0;
+let tieCount = 0;
 
-let playerSelection = prompt("Make your move: ");
-let computerSelection = getComputerChoice().toLowerCase();
-console.log(playRound(playerSelection, computerSelection));
+for (let currentRound = 1; currentRound <= roundsToPlay; currentRound++) {
+    // let playerSelection = prompt("Enter your move: ")
+    let playerSelection = getUserPrompt();
+    if (!possibleMoveArray.includes(playerSelection)) {
+        alert (`Invalid Move : ${playerSelection} Try again`);
+        --currentRound
+        continue;
+    }
 
-playerSelection = prompt("Make your move: ").toLowerCase();
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection, currentRound));
+}
 
-playerSelection = prompt("Make your move: ").toLowerCase();
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
-playerSelection = prompt("Make your move: ").toLowerCase();
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-playerSelection = prompt("Make your move: ").toLowerCase();
-computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-console.log(`Player won ${playerWins}`);
-console.log(`Computer won ${computerWins}`);
 if (computerWins > playerWins) {
     console.log(`The Computer WON`);
 } else {
     console.log(`The Player WON`);
+}
+console.log(`Player won ${playerWins} rounds`);
+console.log(`Computer won ${computerWins}  rounds`);
+if (tieCount > 0) {
+    console.log(`There were ${tieCount} ties`);
 }
